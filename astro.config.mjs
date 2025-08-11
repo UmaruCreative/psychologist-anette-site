877164e70a801c31caae5974d87d651c255c5f5c
UmaruCreative <130234656+UmaruCreative@users.noreply.github.com>
2025-08-11 11:22:12 +0300

    First upload

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+    site: 'https://umarucreative.github.io',
+    base: '/psychologist-anette-site',
+  });
